3db35c117682371fc806624bdbe4ca8f456db66e
So-luf <132810911+Soluf-th@users.noreply.github.com>
2025-12-11 07:47:47 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Analytics from '@vercel/analytics/nuxt'
+import Analytics from '@vercel/analytics/react';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
