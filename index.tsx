4c02cc3a8d15d0a6406108f2c93361720ea6ab33
So-luf <132810911+Soluf-th@users.noreply.github.com>
2025-12-11 08:05:05 +0700

    Update index.tsx
    Signed-off-by: So-luf <132810911+Soluf-th@users.noreply.github.com>

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Analytics from '@vercel/analytics/react';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
